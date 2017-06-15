b5791cc3a642f9c98b1e1dfc64fa84493d5140f4
Ali Araghi <ara@tradeshift.com>
2017-06-15 15:16:49 +0200

    modify test and build file

--- ts-reaktive-akka-cluster/build.sbt
+++ ts-reaktive-akka-cluster/build.sbt
@@ -1,2 +1,18 @@
 
 description := "Provides utility classes that tie Akka clustering into the rest of the libs in ts-reaktive"
+
+// Because of https://github.com/cuppa-framework/cuppa/pull/113
+parallelExecution in Test := false
+
+libraryDependencies ++= {
+  Seq(
+    "junit" % "junit" % "4.11" % "test",
+    "org.assertj" % "assertj-core" % "3.2.0" % "test",
+    "org.mockito" % "mockito-core" % "1.10.19" % "test",
+    "com.novocode" % "junit-interface" % "0.11" % "test",
+    "org.forgerock.cuppa" % "cuppa" % "1.1.0" % "test",
+    "org.forgerock.cuppa" % "cuppa-junit" % "1.1.0" % "test"
+  )
+}
+
+fork in Test := true
--- ts-reaktive-akka-cluster/src/test/java/com/tradeshift/reaktive/akka/cluster/AWSClientSpec.java
+++ ts-reaktive-akka-cluster/src/test/java/com/tradeshift/reaktive/akka/cluster/AWSClientSpec.java
@@ -1,6 +1,8 @@
 package com.tradeshift.reaktive.akka.cluster;
 
-import static com.insightfullogic.lambdabehave.Suite.describe;
+import static org.forgerock.cuppa.Cuppa.beforeEach;
+import static org.forgerock.cuppa.Cuppa.describe;
+import static org.forgerock.cuppa.Cuppa.it;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
@@ -11,6 +13,7 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 
+import org.forgerock.cuppa.junit.CuppaRunner;
 import org.junit.runner.RunWith;
 
 import com.amazonaws.regions.Region;
@@ -28,10 +31,9 @@ import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.InstanceState;
 import com.amazonaws.services.ec2.model.InstanceStateName;
 import com.amazonaws.services.ec2.model.Reservation;
-import com.insightfullogic.lambdabehave.JunitSuiteRunner;
 
-@RunWith(JunitSuiteRunner.class)
-public class AWSClientSpec {{
+@RunWith(CuppaRunner.class)
+public class AWSClientSpec {
 
     final AmazonEC2Client ec2Client = mock(AmazonEC2Client.class);
     final AmazonAutoScalingClient autoScalingClient = mock(AmazonAutoScalingClient.class);
@@ -39,128 +41,134 @@ public class AWSClientSpec {{
     final Region region = mock(Region.class);
     final AWSClient client = new AWSClient(region, ec2Client, autoScalingClient);
 
-    describe("AWSClient.getSiblingIps", it -> {
+    {
+        describe("AWSClient", () -> {
 
-        when(autoScalingClient.describeAutoScalingInstances(eq(new DescribeAutoScalingInstancesRequest().withInstanceIds("instanceId"))))
+
+            beforeEach(() -> {                
+                when(autoScalingClient.describeAutoScalingInstances(eq(new DescribeAutoScalingInstancesRequest().withInstanceIds("instanceId"))))
                 .thenReturn(new DescribeAutoScalingInstancesResult().withAutoScalingInstances(
-                        new AutoScalingInstanceDetails().withAutoScalingGroupName("auto-scaling")));
+                    new AutoScalingInstanceDetails().withAutoScalingGroupName("auto-scaling")));
 
-        when(autoScalingClient.describeAutoScalingGroups(new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames("auto-scaling")))
+                when(autoScalingClient.describeAutoScalingGroups(new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames("auto-scaling")))
                 .thenReturn( new DescribeAutoScalingGroupsResult().withAutoScalingGroups(
-                        new AutoScalingGroup().withInstances(
-                                new com.amazonaws.services.autoscaling.model.Instance().withInstanceId("instanceId"),
-                                new com.amazonaws.services.autoscaling.model.Instance().withInstanceId("instanceId2"),
-                                new com.amazonaws.services.autoscaling.model.Instance().withInstanceId("instanceId3"),
-                                new com.amazonaws.services.autoscaling.model.Instance().withInstanceId("instanceId4"))));
+                    new AutoScalingGroup().withInstances(
+                        new com.amazonaws.services.autoscaling.model.Instance().withInstanceId("instanceId"),
+                        new com.amazonaws.services.autoscaling.model.Instance().withInstanceId("instanceId2"),
+                        new com.amazonaws.services.autoscaling.model.Instance().withInstanceId("instanceId3"),
+                        new com.amazonaws.services.autoscaling.model.Instance().withInstanceId("instanceId4"))));               
+            });
 
-        it.should("return a list of IPs of the running instances, sorted by launch date, excluding own IP if it's not the first launched instance", spec -> {
+            describe("AWSClient.getSiblingIps", () -> {
+                it("should return a list of IPs of the running instances, sorted by launch date, excluding own IP if it's not the first launched instance", () -> {
 
-            when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId"))))
+                    when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId"))))
                     .thenReturn(createInstance("instanceId", "ip", Date.from(Instant.now()), InstanceStateName.Running));
 
-            when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId2"))))
+                    when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId2"))))
                     .thenReturn(createInstance("instanceId2", "ip2", Date.from(Instant.now()), InstanceStateName.Running));
 
-            when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId3"))))
+                    when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId3"))))
                     .thenReturn(createInstance("instanceId3", "ip3", Date.from(Instant.now().minusSeconds(5)), InstanceStateName.Running));
 
-            when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId4"))))
+                    when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId4"))))
                     .thenReturn(createInstance("instanceId4", "ip4", Date.from(Instant.now()), InstanceStateName.Terminated));
 
-            List<String> siblingIps = client.getSiblingIps("instanceId");
+                    List<String> siblingIps = client.getSiblingIps("instanceId");
 
-            assertThat(siblingIps).hasSize(2).containsExactly("ip3", "ip2");
-        });
+                    assertThat(siblingIps).hasSize(2).containsExactly("ip3", "ip2");
+                });
 
-        it.should("return a list of IPs of the running instances,  sorted by launch date, including own IP if it's the first launched instance", spec -> {
+                it("should return a list of IPs of the running instances,  sorted by launch date, including own IP if it's the first launched instance", () -> {
 
-            when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId"))))
+                    when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId"))))
                     .thenReturn(createInstance("instanceId", "ip", Date.from(Instant.now().minusSeconds(10)), InstanceStateName.Running));
 
-            when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId2"))))
+                    when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId2"))))
                     .thenReturn(createInstance("instanceId2", "ip2", Date.from(Instant.now()), InstanceStateName.Running));
 
-            when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId3"))))
+                    when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId3"))))
                     .thenReturn(createInstance("instanceId3", "ip3", Date.from(Instant.now().minusSeconds(5)), InstanceStateName.Running));
 
-            when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId4"))))
+                    when(ec2Client.describeInstances(eq(new DescribeInstancesRequest().withInstanceIds("instanceId4"))))
                     .thenReturn(createInstance("instanceId4", "ip4", Date.from(Instant.now()), InstanceStateName.Terminated));
 
-            List<String> siblingIps = client.getSiblingIps("instanceId");
+                    List<String> siblingIps = client.getSiblingIps("instanceId");
 
-            assertThat(siblingIps).hasSize(3).containsExactly("ip", "ip3", "ip2");
-        });
+                    assertThat(siblingIps).hasSize(3).containsExactly("ip", "ip3", "ip2");
+                });
 
-    });
+            });
 
-    describe("AWSClient.getAutoScalingGroupName", it -> {
-        it.should("return the name of auto-scaling group for a given instanceId", spec -> {
-            DescribeAutoScalingInstancesRequest request = new DescribeAutoScalingInstancesRequest().withInstanceIds("instanceId");
+            describe("AWSClient.getAutoScalingGroupName", () -> {
+                it("should return the name of auto-scaling group for a given instanceId", () -> {
+                    DescribeAutoScalingInstancesRequest request = new DescribeAutoScalingInstancesRequest().withInstanceIds("instanceId");
 
-            DescribeAutoScalingInstancesResult result = new DescribeAutoScalingInstancesResult()
-                    .withAutoScalingInstances(new AutoScalingInstanceDetails().withAutoScalingGroupName("auto-scaling"));
+                    DescribeAutoScalingInstancesResult result = new DescribeAutoScalingInstancesResult()
+                        .withAutoScalingInstances(new AutoScalingInstanceDetails().withAutoScalingGroupName("auto-scaling"));
 
-            when(autoScalingClient.describeAutoScalingInstances(eq(request))).thenReturn(result);
+                    when(autoScalingClient.describeAutoScalingInstances(eq(request))).thenReturn(result);
 
-            String group = client.getAutoScalingGroupName("instanceId");
-            assertThat(group).isEqualTo("auto-scaling");
-        });
+                    String group = client.getAutoScalingGroupName("instanceId");
+                    assertThat(group).isEqualTo("auto-scaling");
+                });
 
-        it.should("throw an exception if instance for a given instanceId belongs to more than 1 auto-scaling group", spec -> {
-            DescribeAutoScalingInstancesRequest request = new DescribeAutoScalingInstancesRequest().withInstanceIds("instanceId");
+                it("should throw an exception if instance for a given instanceId belongs to more than 1 auto-scaling group", () -> {
+                    DescribeAutoScalingInstancesRequest request = new DescribeAutoScalingInstancesRequest().withInstanceIds("instanceId");
 
-            DescribeAutoScalingInstancesResult result = new DescribeAutoScalingInstancesResult()
-                    .withAutoScalingInstances(
+                    DescribeAutoScalingInstancesResult result = new DescribeAutoScalingInstancesResult()
+                        .withAutoScalingInstances(
                             new AutoScalingInstanceDetails().withAutoScalingGroupName("auto-scaling1"),
                             new AutoScalingInstanceDetails().withAutoScalingGroupName("auto-scaling2"));
 
-            when(autoScalingClient.describeAutoScalingInstances(eq(request))).thenReturn(result);
+                    when(autoScalingClient.describeAutoScalingInstances(eq(request))).thenReturn(result);
 
-            assertThatThrownBy(() -> client.getAutoScalingGroupName("instanceId")).isInstanceOf(IllegalStateException.class);
-        });
+                    assertThatThrownBy(() -> client.getAutoScalingGroupName("instanceId")).isInstanceOf(IllegalStateException.class);
+                });
 
-    });
+            });
 
-    describe("AWSClient.getInstance", it -> {
-        it.should("return instance for a given instanceId", spec -> {
-            DescribeInstancesRequest request = new DescribeInstancesRequest().withInstanceIds("instanceId");
+            describe("AWSClient.getInstance", () -> {
+                it("should return instance for a given instanceId", () -> {
+                    DescribeInstancesRequest request = new DescribeInstancesRequest().withInstanceIds("instanceId");
 
-            DescribeInstancesResult result = new DescribeInstancesResult()
-                    .withReservations(new Reservation()
+                    DescribeInstancesResult result = new DescribeInstancesResult()
+                        .withReservations(new Reservation()
                             .withInstances(new Instance().withInstanceId("instanceId")));
 
-            when(ec2Client.describeInstances(eq(request))).thenReturn(result);
+                    when(ec2Client.describeInstances(eq(request))).thenReturn(result);
 
-            Instance instance = client.getInstance("instanceId");
-            assertThat(instance).isNotNull();
-            assertThat(instance.getInstanceId()).isEqualTo("instanceId");
-        });
-    });
+                    Instance instance = client.getInstance("instanceId");
+                    assertThat(instance).isNotNull();
+                    assertThat(instance.getInstanceId()).isEqualTo("instanceId");
+                });
+            });
 
-    describe("AWSClient.getAutoScalingGroupInstanceIds", it -> {
-        it.should("return a list of instanceIds for a given auto-scaling group name", spec -> {
-            DescribeAutoScalingGroupsRequest request = new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames("groupName");
+            describe("AWSClient.getAutoScalingGroupInstanceIds", () -> {
+                it("should return a list of instanceIds for a given auto-scaling group name", () -> {
+                    DescribeAutoScalingGroupsRequest request = new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames("groupName");
 
-            DescribeAutoScalingGroupsResult result = new DescribeAutoScalingGroupsResult().withAutoScalingGroups(
-                    new AutoScalingGroup().withInstances(
+                    DescribeAutoScalingGroupsResult result = new DescribeAutoScalingGroupsResult().withAutoScalingGroups(
+                        new AutoScalingGroup().withInstances(
                             new com.amazonaws.services.autoscaling.model.Instance(),
                             new com.amazonaws.services.autoscaling.model.Instance()
-                    ));
+                            ));
 
-            when(autoScalingClient.describeAutoScalingGroups(request)).thenReturn(result);
+                    when(autoScalingClient.describeAutoScalingGroups(request)).thenReturn(result);
 
-            List<String> groupInstanceIds = client.getAutoScalingGroupInstanceIds("groupName");
-            assertThat(groupInstanceIds).hasSize(2);
+                    List<String> groupInstanceIds = client.getAutoScalingGroupInstanceIds("groupName");
+                    assertThat(groupInstanceIds).hasSize(2);
+                });
+            });
         });
-    });
-}
+    }
 
     private DescribeInstancesResult createInstance(String instanceId, String privateIp, Date launchDate, InstanceStateName state) {
         return new DescribeInstancesResult().withReservations(
-                new Reservation().withInstances(new Instance()
-                        .withInstanceId(instanceId)
-                        .withPrivateIpAddress(privateIp)
-                        .withLaunchTime(launchDate)
-                        .withState(new InstanceState().withName(state))));
+            new Reservation().withInstances(new Instance()
+                .withInstanceId(instanceId)
+                .withPrivateIpAddress(privateIp)
+                .withLaunchTime(launchDate)
+                .withState(new InstanceState().withName(state))));
     }
 }
